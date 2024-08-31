3b92fbd816de2ac21f14a3e6aff699ab3daf111f
JotaCeh <93413256+JotaCeh@users.noreply.github.com>
2024-08-31 11:44:27 -0600

    Update UsuarioPregunta.cs

--- SistemaSeguridad/Models/UsuarioPregunta.cs
+++ SistemaSeguridad/Models/UsuarioPregunta.cs
@@ -1,4 +1,6 @@
-﻿namespace SistemaSeguridad.Models
+using System.ComponentModel.DataAnnotations;
+
+namespace SistemaSeguridad.Models
 {
 	public class UsuarioPregunta
 	{
@@ -6,7 +8,8 @@
         public string IdUsuario { get; set; }
         public string Pregunta { get; set; }
         public string Respuesta { get; set; }
-        public int OrdenPregunta { get; set; }
+		[Required(ErrorMessage = "El campo {0} es requerido")]
+		public int OrdenPregunta { get; set; }
         public DateTime FechaCreacion { get; set; }
 		public string UsuarioCreacion { get; set; }
         public DateTime FechaModificacion { get; set; }
