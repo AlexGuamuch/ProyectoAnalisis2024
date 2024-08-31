5bc334a92d88e661a1f54db950865e897a02b076
Rodmxn <116456325+Rodmxxn@users.noreply.github.com>
2024-08-31 12:20:49 -0600

    Update GeneroController.cs

--- SistemaSeguridad/Controllers/GeneroController.cs
+++ SistemaSeguridad/Controllers/GeneroController.cs
@@ -7,134 +7,129 @@ using System.Threading.Tasks;
 
 namespace SistemaSeguridad.Controllers
 {
-	public class GeneroController: Controller
-	{
+    public class GeneroController : Controller
+    {
         private readonly IRepositoryGenero repositoryGenero;
         private readonly IServicioUsuarios servicioUsuarios;
 
         public GeneroController(IRepositoryGenero repositoryGenero,
-			IServicioUsuarios servicioUsuarios) 
-		{ 
-			this.repositoryGenero = repositoryGenero;
-			this.servicioUsuarios = servicioUsuarios;
-		}
+            IServicioUsuarios servicioUsuarios)
+        {
+            this.repositoryGenero = repositoryGenero;
+            this.servicioUsuarios = servicioUsuarios;
+        }
 
-        public async Task<IActionResult> Index() 
-		{
+        public async Task<IActionResult> Index()
+        {
+            var genero = await repositoryGenero.Obtener();
+            return View(genero);
+        }
 
-			var genero = await repositoryGenero.Obtener();
-			return View(genero);	
-		}
+        public IActionResult Crear()
+        {
+            return View();
+        }
 
-		public IActionResult Crear() 
-		{
-			return View();
-		}
+        [HttpPost]
+        public async Task<IActionResult> Crear(Genero genero)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(genero);
+            }
 
-		[HttpPost]
-		public async Task<IActionResult> Crear(Genero genero) 
-		{
-			if (!ModelState.IsValid)
-			{
-				return View(genero);
-			}
-
-			genero.UsuarioCreacion = servicioUsuarios.ObtenerUsuarioId();
+            genero.UsuarioCreacion = servicioUsuarios.ObtenerUsuarioId();
 
             var existeGenero = await repositoryGenero.Existe(genero.Nombre);
 
-			if (existeGenero) 
-			{
-				ModelState.AddModelError(nameof(genero.Nombre), $"El nombre {genero.Nombre} ya existe");
-				return View(genero);
-			}
+            if (existeGenero)
+            {
+                ModelState.AddModelError(nameof(genero.Nombre), $"El nombre {genero.Nombre} ya existe");
+                return View(genero);
+            }
             await repositoryGenero.Crear(genero);
 
-			return RedirectToAction("Index");
-		}
-
-		[HttpGet]
-		public async Task<ActionResult> Editar(int IdGenero) 
-		{
-			var generotipo = await repositoryGenero.ObtenerPorId(IdGenero);
-
-			if (generotipo is null) 
-			{ 
-				return RedirectToAction("Index", "Home");
-			}
-
-			return View(generotipo);
-		}
-
-        [HttpPost]
-		public async Task<ActionResult> Editar(Genero genero) 
-		{
-			genero.UsuarioModificacion = servicioUsuarios.ObtenerUsuarioId();
-			var tipocuentaExiste = await repositoryGenero.ObtenerPorId(genero.IdGenero);
-
-			if (tipocuentaExiste is null) 
-			{ 
-				return RedirectToAction("Index", "Home");
-			}
-			await repositoryGenero.Actualizar(genero);
             return RedirectToAction("Index");
-		}
+        }
 
-		public async Task<IActionResult> Borrar(int idGenero) 
-		{
-			var genero = await repositoryGenero.ObtenerPorId(idGenero);
+        [HttpGet]
+        public async Task<ActionResult> Editar(int IdGenero)
+        {
+            var generotipo = await repositoryGenero.ObtenerPorId(IdGenero);
 
-			if (genero is null)		
-			{
+            if (generotipo is null)
+            {
                 return RedirectToAction("Index", "Home");
             }
-			return View(genero);
-		}
 
-		[HttpPost]
-		public async Task<IActionResult> BorrarGenero(int idGenero) 
-		{
+            return View(generotipo);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Editar(Genero genero)
+        {
+            genero.UsuarioModificacion = servicioUsuarios.ObtenerUsuarioId();
+            var tipocuentaExiste = await repositoryGenero.ObtenerPorId(genero.IdGenero);
+
+            if (tipocuentaExiste is null)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            await repositoryGenero.Actualizar(genero);
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> Borrar(int idGenero)
+        {
+            var genero = await repositoryGenero.ObtenerPorId(idGenero);
+
+            if (genero is null)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            return View(genero);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> BorrarGenero(int idGenero)
+        {
             var genero = await repositoryGenero.ObtenerPorId(idGenero);
             if (genero is null)
             {
                 return RedirectToAction("Index", "Home");
             }
-			await repositoryGenero.Borrar(idGenero);
-			return RedirectToAction("Index");
-
+            await repositoryGenero.Borrar(idGenero);
+            return RedirectToAction("Index");
         }
 
+        [HttpGet]
+        public async Task<IActionResult> VerifarGenero(string nombre)
+        {
+            var existeGenero = await repositoryGenero.Existe(nombre);
+            if (existeGenero)
+            {
+                return Json($"El nombre {nombre} ya existe");
+            }
 
-         [HttpGet]
-		public async Task<IActionResult> VerifarGenero(string nombre) 
-		{ 
-			var existeGenero = await repositoryGenero.Existe(nombre);
-			if (existeGenero) 
-			{
-				return Json($"El nombre {nombre} ya existe");
-			}
+            return Json(true);
+        }
 
-			return Json(true);
-		}
+        // funcion para exportar a CSV
+        [HttpGet]
 
-		    // funcion para exportar a CSV
- 	   [HttpGet]
+        public async Task<IActionResult> ExportarCSV()
+        {
+            var generos = await repositoryGenero.Obtener();
+            var csv = new StringBuilder();
+            csv.AppendLine("Nombre, IdGenero"); // Encabezado de la columna
 
-	    public async Task<IActionResult> ExportarCSV()
-  	  {
-    	    var generos = await repositoryGenero.Obtener();
-    	    var csv = new StringBuilder();
-     		   csv.AppendLine("Nombre, IdGenero"); // Encabezado de la columna
+            foreach (var genero in generos)
+            {
+                csv.AppendLine($"{genero.IdGenero},{genero.Nombre}"); // Datos del CSV
+            }
 
-      		  foreach (var genero in generos)
-       		 {
-        	    csv.AppendLine($"{genero.IdGenero},{genero.Nombre}"); // Datos del CSV
-       		 }
-
-      	  var bytes = Encoding.UTF8.GetBytes(csv.ToString());
-      	  return File(bytes, "text/csv", "generos.csv");
+            var bytes = Encoding.UTF8.GetBytes(csv.ToString());
+            return File(bytes, "text/csv", "generos.csv");
+        }
     }
-}
-   
-	}
 }
