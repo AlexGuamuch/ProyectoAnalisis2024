a5ad40d3b6423d6db7bb650fd9f1d6884bdd94c4
Rodmxn <116456325+Rodmxxn@users.noreply.github.com>
2024-10-18 21:14:18 -0600

    Add files via upload

---
+++ SistemaSeguridad/Models/EstadoCivil.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SistemaSeguridad.Models;
+
+public partial class EstadoCivil
+{
+    public int IdEstadoCivil { get; set; }
+
+    public string Nombre { get; set; }
+
+    public DateTime FechaCreacion { get; set; }
+
+    public string UsuarioCreacion { get; set; }
+
+    public DateTime? FechaModificacion { get; set; }
+
+    public string UsuarioModificacion { get; set; }
+
+    public virtual ICollection<Persona> Personas { get; set; } = new List<Persona>();
+}
---
+++ SistemaSeguridad/Models/Persona.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SistemaSeguridad.Models;
+
+public partial class Persona
+{
+    public int IdPersona { get; set; }
+
+    public string Nombre { get; set; }
+
+    public string Apellido { get; set; }
+
+    public DateTime FechaNacimiento { get; set; }
+
+    public int IdGenero { get; set; }
+
+    public string Direccion { get; set; }
+
+    public string Telefono { get; set; }
+
+    public string CorreoElectronico { get; set; }
+
+    public int IdEstadoCivil { get; set; }
+
+    public DateTime FechaCreacion { get; set; }
+
+    public string UsuarioCreacion { get; set; }
+
+    public DateTime? FechaModificacion { get; set; }
+
+    public string UsuarioModificacion { get; set; }
+
+    public virtual EstadoCivil IdEstadoCivilNavigation { get; set; }
+
+    public virtual ICollection<SaldoCuentum> SaldoCuenta { get; set; } = new List<SaldoCuentum>();
+}
