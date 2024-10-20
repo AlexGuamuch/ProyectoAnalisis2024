97694421fb7f11cd8ed85d3c903a69102eecaacc
Rodmxn <116456325+Rodmxxn@users.noreply.github.com>
2024-10-19 23:27:26 -0600

    Add files via upload

---
+++ SistemaSeguridad/Controllers/TipoMovimientoCxcController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SistemaSeguridad.Servicios;
+using SistemaSeguridad.Models;
+
+namespace SistemaSeguridad.Controllers
+{
+    public class TipoMovimientoCxcController : Controller
+    {
+        private readonly IRepositoryTipoMovimientoCxc _repository;
+
+        public TipoMovimientoCxcController(IRepositoryTipoMovimientoCxc repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var tiposMovimiento = await _repository.ObtenerTodos();
+            return View(tiposMovimiento);
+        }
+
+        public async Task<IActionResult> Detalles(int id)
+        {
+            var tipoMovimiento = await _repository.ObtenerPorId(id);
+            if (tipoMovimiento == null)
+            {
+                return NotFound();
+            }
+            return View(tipoMovimiento);
+        }
+    }
+}
---
+++ SistemaSeguridad/Controllers/TipoSaldoCuentumController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SistemaSeguridad.Servicios;
+using SistemaSeguridad.Models;
+
+namespace SistemaSeguridad.Controllers
+{
+    public class TipoSaldoCuentumController : Controller
+    {
+        private readonly IRepositoryTipoSaldoCuentum _repository;
+
+        public TipoSaldoCuentumController(IRepositoryTipoSaldoCuentum repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var tiposSaldo = await _repository.ObtenerTodos();
+            return View(tiposSaldo);
+        }
+
+        public async Task<IActionResult> Detalles(int id)
+        {
+            var tipoSaldo = await _repository.ObtenerPorId(id);
+            if (tipoSaldo == null)
+            {
+                return NotFound();
+            }
+            return View(tipoSaldo);
+        }
+    }
+}
