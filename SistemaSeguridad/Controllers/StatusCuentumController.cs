4c59e2cefa6c7723541aacecbac83b2f5cc9c30f
Rodmxn <116456325+Rodmxxn@users.noreply.github.com>
2024-10-19 21:50:19 -0600

    Add files via upload

---
+++ SistemaSeguridad/Controllers/StatusCuentumController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SistemaSeguridad.Models;
+using SistemaSeguridad.Servicios;
+
+namespace SistemaSeguridad.Controllers
+{
+    public class StatusCuentumController : Controller
+    {
+        private readonly IRepositoryStatusCuentum _repository;
+
+        public StatusCuentumController(IRepositoryStatusCuentum repository)
+        {
+            _repository = repository;
+        }
+
+        // GET: StatusCuentum
+        public async Task<IActionResult> Index()
+        {
+            var statusCuentas = await _repository.ObtenerTodos();
+            return View(statusCuentas);
+        }
+
+        // GET: StatusCuentum/5
+        public async Task<IActionResult> Detalles(int id)
+        {
+            var statusCuenta = await _repository.ObtenerPorId(id);
+            if (statusCuenta == null)
+            {
+                return NotFound();
+            }
+            return View(statusCuenta);
+        }
+    }
+}
---
+++ SistemaSeguridad/Controllers/TipoDocumentoController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SistemaSeguridad.Models;
+using SistemaSeguridad.Servicios;
+
+namespace SistemaSeguridad.Controllers
+{
+    public class TipoDocumentoController : Controller
+    {
+        private readonly IRepositoryTipoDocumento repositoryTipoDocumento;
+
+        public TipoDocumentoController(IRepositoryTipoDocumento repositoryTipoDocumento)
+        {
+            this.repositoryTipoDocumento = repositoryTipoDocumento;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var tiposDocumento = await repositoryTipoDocumento.ObtenerTodos();
+            return View(tiposDocumento);
+        }
+
+        public async Task<IActionResult> Detalles(int idTipoDocumento)
+        {
+            var tipoDocumento = await repositoryTipoDocumento.ObtenerPorId(idTipoDocumento);
+            if (tipoDocumento is null)
+            {
+                return NotFound();
+            }
+            return View(tipoDocumento);
+        }
+    }
+}
