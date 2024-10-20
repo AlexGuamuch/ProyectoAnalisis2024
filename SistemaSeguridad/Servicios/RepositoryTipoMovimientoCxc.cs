03b396c2e7373d94534e933c1f786d00fe596e1d
Rodmxn <116456325+Rodmxxn@users.noreply.github.com>
2024-10-19 23:29:17 -0600

    Add files via upload

---
+++ SistemaSeguridad/Servicios/RepositoryTipoMovimientoCxc.cs
@@ -0,0 +1,42 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using SistemaSeguridad.Models;
+
+namespace SistemaSeguridad.Servicios
+{
+    public interface IRepositoryTipoMovimientoCxc
+    {
+        Task<IEnumerable<TipoMovimientoCxc>> ObtenerTodos();
+        Task<TipoMovimientoCxc> ObtenerPorId(int idTipoMovimientoCxc);
+    }
+
+    public class RepositoryTipoMovimientoCxc : IRepositoryTipoMovimientoCxc
+    {
+        private readonly string connectionString;
+
+        public RepositoryTipoMovimientoCxc(IConfiguration configuration)
+        {
+            connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+
+        public async Task<IEnumerable<TipoMovimientoCxc>> ObtenerTodos()
+        {
+            using var connection = new SqlConnection(connectionString);
+            return await connection.QueryAsync<TipoMovimientoCxc>(@"
+                SELECT IdTipoMovimientoCxc, Nombre, OperacionCuentaCorriente, 
+                       FechaCreacion, UsuarioCreacion, FechaModificacion, UsuarioModificacion
+                FROM TIPO_MOVIMIENTO_CXC;");
+        }
+
+        public async Task<TipoMovimientoCxc> ObtenerPorId(int idTipoMovimientoCxc)
+        {
+            using var connection = new SqlConnection(connectionString);
+            return await connection.QueryFirstOrDefaultAsync<TipoMovimientoCxc>(@"
+                SELECT IdTipoMovimientoCxc, Nombre, OperacionCuentaCorriente, 
+                       FechaCreacion, UsuarioCreacion, FechaModificacion, UsuarioModificacion
+                FROM TIPO_MOVIMIENTO_CXC 
+                WHERE IdTipoMovimientoCxc = @IdTipoMovimientoCxc;",
+                new { IdTipoMovimientoCxc = idTipoMovimientoCxc });
+        }
+    }
+}
---
+++ SistemaSeguridad/Servicios/RepositoryTipoSaldoCuentum.cs
@@ -0,0 +1,42 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using SistemaSeguridad.Models;
+
+namespace SistemaSeguridad.Servicios
+{
+    public interface IRepositoryTipoSaldoCuentum
+    {
+        Task<IEnumerable<TipoSaldoCuentum>> ObtenerTodos();
+        Task<TipoSaldoCuentum> ObtenerPorId(int idTipoSaldoCuenta);
+    }
+
+    public class RepositoryTipoSaldoCuentum : IRepositoryTipoSaldoCuentum
+    {
+        private readonly string connectionString;
+
+        public RepositoryTipoSaldoCuentum(IConfiguration configuration)
+        {
+            connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+
+        public async Task<IEnumerable<TipoSaldoCuentum>> ObtenerTodos()
+        {
+            using var connection = new SqlConnection(connectionString);
+            return await connection.QueryAsync<TipoSaldoCuentum>(@"
+                SELECT IdTipoSaldoCuenta, Nombre, 
+                       FechaCreacion, UsuarioCreacion, FechaModificacion, UsuarioModificacion
+                FROM TIPO_SALDO_CUENTA;");
+        }
+
+        public async Task<TipoSaldoCuentum> ObtenerPorId(int idTipoSaldoCuenta)
+        {
+            using var connection = new SqlConnection(connectionString);
+            return await connection.QueryFirstOrDefaultAsync<TipoSaldoCuentum>(@"
+                SELECT IdTipoSaldoCuenta, Nombre, 
+                       FechaCreacion, UsuarioCreacion, FechaModificacion, UsuarioModificacion
+                FROM TIPO_SALDO_CUENTA 
+                WHERE IdTipoSaldoCuenta = @IdTipoSaldoCuenta;",
+                new { IdTipoSaldoCuenta = idTipoSaldoCuenta });
+        }
+    }
+}
