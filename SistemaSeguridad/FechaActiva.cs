dd350ec34c36c1ec5d86215169d91f2381b39300
Rodmxn <116456325+Rodmxxn@users.noreply.github.com>
2024-10-19 21:52:53 -0600

    Add files via upload

---
+++ SistemaSeguridad/FechaActiva.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SistemaSeguridad.Models;
+
+public partial class FechaActiva
+{
+    public DateOnly FechaInicial { get; set; }
+
+    public DateOnly FechaFinal { get; set; }
+}
