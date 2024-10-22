e8e759ae4a90c2e9785aebb2a2114480953e1d2b
Rodmxn <116456325+Rodmxxn@users.noreply.github.com>
2024-10-21 19:17:34 -0600

    Add files via upload

---
+++ SistemaSeguridad/Controllers/EstadoCivilController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SistemaSeguridad.Servicios;
+
+namespace SistemaSeguridad.Controllers
+{
+    public class EstadoCivilController : Controller
+    {
+        private readonly IRepositoryEstadoCivil _repository;
+
+        public EstadoCivilController(IRepositoryEstadoCivil repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var estadosCiviles = await _repository.ObtenerTodos();
+            return View(estadosCiviles);
+        }
+
+        public async Task<IActionResult> Detalle(int id)
+        {
+            var estadoCivil = await _repository.ObtenerPorId(id);
+            if (estadoCivil == null) return NotFound();
+            return View(estadoCivil);
+        }
+    }
+}
