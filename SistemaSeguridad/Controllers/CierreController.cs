4f16576e5ca18a8f57cc9ea2fd30025c1b874647
pbarriosa2199 <149257021+pbarriosa2199@users.noreply.github.com>
2024-11-10 16:42:58 -0600

    Add files via upload

---
+++ SistemaSeguridad/Controllers/CierreController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SistemaSeguridad.Servicios;
+
+namespace SistemaSeguridad.Controllers
+{
+    public class CierreController : Controller
+    {
+        private readonly CierreService _cierreService; 
+        public CierreController(CierreService cierreService) 
+        { _cierreService = cierreService; }
+        
+        [HttpPost]
+        public async Task<IActionResult> EjecutarCierre(DateTime fechaInicio, DateTime fechaCierre)
+        { 
+            await _cierreService.EjecutarProcedimientoCierre(fechaInicio, fechaCierre); 
+            return Json(new { success = true, message = "El cierre se realizó con éxito." }); 
+        }
+    }
+}
